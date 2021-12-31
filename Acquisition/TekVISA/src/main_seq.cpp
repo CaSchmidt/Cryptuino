c84481c8fd1e9bcef28b40b4b9f4a50eecc11cbb
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2021-12-31 16:58:29 +0100

    Integrate instrument control.

--- Acquisition/TekVISA/src/main_seq.cpp
+++ Acquisition/TekVISA/src/main_seq.cpp
@@ -32,15 +32,26 @@
 #include <cstdio>
 #include <cstdlib>
 
+#include <array>
 #include <charconv>
 #include <format>
 #include <string>
+
+#define HAVE_INSTRUMENT
+
+#ifdef HAVE_INSTRUMENT
+# include <matio.h>
+#endif
 
 #include <csUtil/csDualLogger.h>
 #include <csUtil/csLogger.h>
 #include <csUtil/csSerial.h>
 #include <csUtil/csStringUtil.h>
+#include <csUtil/csTime.h>
 
+#ifdef HAVE_INSTRUMENT
+# include "instrument.h"
+#endif
 #include "util.h"
 
 template<typename T>
@@ -123,9 +134,127 @@ bool parseOptions(Options& options, int argc, char **argv)
   return options.isValid();
 }
 
+template<typename... Args>
+inline void logText(const csILogger *logger, const char8_t *fmt, Args&&... args)
+{
+  const std::string msg = std::vformat(cs::CSTR(fmt), std::make_format_args(args...));
+  logger->logText(cs::UTF8(msg.data()));
+}
+
+template<typename... Args>
+inline void logError(const csILogger *logger, const char8_t *fmt, Args&&... args)
+{
+  const std::string msg = std::vformat(cs::CSTR(fmt), std::make_format_args(args...));
+  logger->logError(cs::UTF8(msg.data()));
+}
+
+#ifdef HAVE_INSTRUMENT
+bool writeMatVector(const csILogger *logger, mat_t *file,
+                    const std::string& varname, const std::size_t numSamples, const double *data)
+{
+  std::array<std::size_t,2> dims;
+  dims[0] = numSamples;
+  dims[1] = 1;
+
+  matvar_t *var = Mat_VarCreate(varname.data(), MAT_C_DOUBLE, MAT_T_DOUBLE,
+                                int(dims.size()), dims.data(), const_cast<double*>(data),
+                                MAT_F_DONT_COPY_DATA);
+  if( var == NULL ) {
+    logError(logger, u8"Mat_VarCreate({})", varname);
+    return false;
+  }
+
+  const int err = Mat_VarWrite(file, var, MAT_COMPRESSION_ZLIB);
+  Mat_VarFree(var);
+
+  return err == 0;
+}
+
+bool writeMatSamples(const csILogger *logger, mat_t *matfile,
+                     const std::string& varname, const SampleBuffer& samples)
+{
+  // (1) Sanity check ////////////////////////////////////////////////////////
+
+  if( samples.size() < 3 ) {
+    logError(logger, u8"No samples for \"{}\"!", varname);
+    return false;
+  }
+  const SampleBuffer::size_type numSamples = samples.size() - 2;
+
+  // (2) Time vector /////////////////////////////////////////////////////////
+
+  SampleBuffer time;
+  try {
+    time.resize(numSamples, 0);
+  } catch (...) {
+    logError(logger, u8"Unable to create time data for \"{}\"!", varname);
+    return false;
+  }
+
+  const double xIncr = samples[0];
+  const double xZero = samples[1];
+  for(SampleBuffer::size_type i = 0; i < time.size(); i++) {
+    time[i] = double(i)*xIncr + xZero;
+  }
+
+  // (3) Output //////////////////////////////////////////////////////////////
+
+  if( !writeMatVector(logger, matfile, varname, numSamples, samples.data() + 2) ) {
+    logError(logger, u8"Unable to write data for \"{}\"!", varname);
+    return false;
+  }
+  if( !writeMatVector(logger, matfile, "t_" + varname, numSamples, time.data()) ) {
+    logError(logger, u8"Unable to write time data for \"{}\"!", varname);
+    return false;
+  }
+
+  return true;
+}
+
+bool writeMatOutput(const csILogger *logger, ViSession vi,
+                    const std::string& filename)
+{
+  constexpr char CH_TRACE   = '1';
+  constexpr char CH_TRIGGER = '2';
+
+  mat_t *matfile = Mat_CreateVer(filename.data(), NULL, MAT_FT_DEFAULT);
+  if( matfile == NULL ) {
+    return false;
+  }
+  ScopeGuard guard([&](void) -> void {
+        if( matfile != NULL ) {
+          Mat_Close(matfile);
+          matfile = NULL;
+        }
+  });
+
+  SampleBuffer trace;
+  if( !readWaveform(logger, vi, CH_TRACE, 0, trace) ) {
+    return false;
+  }
+  if( !writeMatSamples(logger, matfile, "trace", trace) ) {
+    return false;
+  }
+
+  SampleBuffer trigger;
+  if( !readWaveform(logger, vi, CH_TRIGGER, 0, trigger) ) {
+    return false;
+  }
+  if( !writeMatSamples(logger, matfile, "trigger", trigger) ) {
+    return false;
+  }
+
+  logText(logger, u8"Wrote file \"{}\".", filename);
+
+  return true;
+}
+#endif
+
 int main(int argc, char **argv)
 {
+#ifdef HAVE_INSTRUMENT
   constexpr unsigned int TOUT_INSTRUMENT = 2;
+#endif
   constexpr unsigned int TOUT_SERIAL = 2;
 
   // (1) Options /////////////////////////////////////////////////////////////
@@ -153,13 +282,49 @@ int main(int argc, char **argv)
 
   // (3) Setup ///////////////////////////////////////////////////////////////
 
+#ifdef HAVE_INSTRUMENT
+  ViStatus status;
+
+  ViSession rm;
+  status = viOpenDefaultRM(&rm);
+  if( handleError(logger, rm, status, "viOpenDefaultRM()") ) {
+    return EXIT_FAILURE;
+  }
+  ScopeGuard guard_rm([&](void) -> void {
+    viClose(rm);
+  });
+
+  const RsrcList resources = queryInstruments(logger, rm);
+  if( resources.empty() ) {
+    logger->logError(u8"No instruments found!");
+    return EXIT_FAILURE;
+  }
+  const std::string instrument = resources.front(); // pick one...
+
+  ViSession vi;
+  status = viOpen(rm, const_cast<char*>(instrument.data()), VI_NULL, VI_NULL, &vi);
+  if( handleError(logger, rm, status, "viOpen()") ) {
+    return EXIT_FAILURE;
+  }
+
+  ViUInt32 length = 0;
+  if( !queryRecordLength(logger, vi, &length) ) {
+    return EXIT_FAILURE;
+  }
+  logText(logger, u8"HORizontal:RECOrdlength = {}", length);
+
+  float rate = 0;
+  if( !querySampleRate(logger, vi, &rate) ) {
+    return EXIT_FAILURE;
+  }
+  logText(logger, u8"HORizontal:SAMPLERate = {}", rate);
+#endif
+
   Randomizer randomizer;
 
   csSerial serial;
   if( !serial.open(cs::UTF8(options.serialDevice.data()), options.serialRate) ) {
-    const std::string msg = std::format("Unable to open serial device \"{}\"!",
-                                        options.serialDevice);
-    logger->logError(cs::UTF8(msg.data()));
+    logError(logger, u8"Unable to open serial device \"{}\"!", options.serialDevice);
     return EXIT_FAILURE;
   }
 
@@ -171,16 +336,32 @@ int main(int argc, char **argv)
   const int maxIter       = options.count - 1;
   const int numIterDigits = countDigits(maxIter);
   for(int i = 0; i <= maxIter; i++) {
-    const std::string progress = std::format("{:0{}}/{}", i, numIterDigits, maxIter);
-    logger->logText(cs::UTF8(progress.data()));
+    logText(logger, u8"{:0{}}/{}", i, numIterDigits, maxIter);
+
+    // (4.1) Arm instrument //////////////////////////////////////////////////
+
+#ifdef HAVE_INSTRUMENT
+    setSingleShotAcquisition(logger, vi);
+    csSleep(TOUT_INSTRUMENT);
+#endif
+
+    // (4.2) Request encryption //////////////////////////////////////////////
 
     txAesCmd('#', serial, randomizer);
+
+    // (4.3) Wait for response ///////////////////////////////////////////////
+
     rxAesCmd(logger, serial, TOUT_SERIAL);
+
+    // (4.4) Store samples ///////////////////////////////////////////////////
+
+#ifdef HAVE_INSTRUMENT
+    const std::string filename = std::format("{:0{}}.mat", i, numIterDigits);
+    writeMatOutput(logger, vi, filename);
+#endif
   }
 
   // Done! ///////////////////////////////////////////////////////////////////
-
-  fclose(file);
 
   return EXIT_SUCCESS;
 }
