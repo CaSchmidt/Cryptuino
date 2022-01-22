9d4e692b11102b90b2bcb41ba1c3442a088ad8eb
Carsten Schmidt <CaSchmidt@users.noreply.github.com>
2022-01-22 11:20:07 +0100

    Extend interface of instrument abstraction.

--- Cryptuino/Libraries/AcquisitionUtil/include/IInstrument.h
+++ Cryptuino/Libraries/AcquisitionUtil/include/IInstrument.h
@@ -46,8 +46,14 @@ class IInstrument {
 public:
   virtual ~IInstrument() noexcept;
 
+  virtual bool connect(const csILogger *logger) = 0;
+  virtual void disconnect() = 0;
+  virtual bool isConnected() const = 0;
+
   virtual bool readSamples(const csILogger *logger, const int channel,
                            SampleBuffer *samples, const std::size_t numSamplesWant = 0) const = 0;
+
+  virtual bool setupTrigger(const csILogger *logger, const unsigned int tout) const = 0;
 
 protected:
   IInstrument() noexcept;
